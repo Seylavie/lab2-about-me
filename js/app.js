b20b69e1bc6c7be07b01036cf7e2c241bbbdd146
Kara Hunte <ncryted@gmail.com>
2019-04-18 19:17:07 -0400

    index.html file 1st attempt

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-<strict
+var kara = {
+    name: "kara",
+    likes: "cheese",
+    height: 162,
+    hobbies:["reading","writing","creative arts"],
+    drives: true,
+    listHobbies: function(){
+      for ( var i= 0;  i < this.hobbies.length;i=1+i){
+        console.log(this.hobbies[i]);
+      }
+      
+      }for (var i = 0; i < students.length; i++) {
+        if (this.students[i].height == 163) {
+          console.log(students[i].name + "'s hobbies are " + students[i].hobbies);
+          // this.students.listHobbies();
+        }
+        else console.log(students[i].name + " is not the right height" )
+      }
+      
+      
+var students = [];
+
+students.push(maria);
+students.push(mona);
+students.push(halle);
+students.push(kara);
+students.push(damien);
