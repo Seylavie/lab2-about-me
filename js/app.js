e5f8ed00bdb980c1fc7fd9eedef3a41ac81ebef4
Kara Hunte <ncryted@gmail.com>
2019-04-20 10:08:25 -0400

    added game and portfolio linked pages

--- js/app.js
+++ js/app.js
@@ -1,5 +1,46 @@
-var kara = {
-    name: "kara",
+'use strict';
+
+var queries = prompt ('Would you like to play a game to get to know me better? yes? no? not sure?').lowerCase();
+console.log(queries);
+var answers = ['yes','y','no','n'].lowerCase();
+
+ if(answers === 'yes'){ 
+     alert ('Let\'s play!');
+ } else if ( answers ==='no'){
+     alert( 'Oh dear! Maybe next time?');
+      } else {
+          alert ('Try it! Just for fun!');
+      }  
+
+switch(queries) {
+    case'y':
+    case 'yes':
+        alert ('Great! Then let\'s play a game?');
+        break;
+    case'n':
+    case'no':
+        alert( 'Okay, Maybe next time?');
+        break;
+        default:
+        alert('It couldn\'t hurt to try!');
+        alert('Wanna Try?');
+    case'y':
+    case'yes':
+        alert('Great let\'s start!');
+        break;
+    case'n':
+    case'no':
+        alert('Guess it wasn\'t meant to be!');
+        break;
+}
+
+var myHobbies = ['reading', 'Watching TV','Drawing','Listening to Music','Writing', 'Travelling',
+                'Dancing','Singing','Cooking','Baking','Coding','Exploring New Things','Learning New Things'];
+for (var i = 0; i < myHobbies.length; i++) {
+    console.log("I like to " + [i]);
+}
+
+  /*  name: "kara",
     likes: "cheese",
     height: 162,
     hobbies:["reading","writing","creative arts"],
@@ -17,11 +58,10 @@ var kara = {
         else console.log(students[i].name + " is not the right height" )
       }
       
-      
-var students = [];
+        
+var queries = [1,2];
 
-students.push(maria);
-students.push(mona);
-students.push(halle);
-students.push(kara);
-students.push(damien);
+if (queries=== No, NO, N, n) 
+{ 
+ console.log("You're right");
+}
